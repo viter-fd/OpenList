5342ae96d0848786708023975bb594435e9b837c
pikachuim <pikachuim@qq.com>
2025-08-14 21:39:00 +0800

    add(trunk): base interface

---
+++ layers/keys/keyset.go
@@ -0,0 +1,22 @@
+package keys
+
+type UserKeys struct {
+	uuid    string   // 密钥UUID
+	user    string   // 所属用户
+	main    string   // 核心密钥（用户密钥SHA2）
+	name    string   // 友好名称
+	algo    int8     // 密钥算法
+	enabled bool     // 是否启用
+	encFile bool     // 加密文件
+	encName bool     // 加密名称
+	keyAuto bool     // 自动更新
+	keyRand bool     // 随机密钥
+	keyAuth UserAuth // 密钥认证
+}
+
+type UserAuth struct {
+	uuid   string // 密钥UUID
+	user   string // 所属用户
+	plugin string // 认证插件
+	config string // 认证配置
+}
